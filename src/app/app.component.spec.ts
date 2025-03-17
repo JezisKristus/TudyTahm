faf2930032c114dc2a2f90d99606f964cb67a07a
vlasakantonin <vlasaktonda11@gmail.com>
2025-03-17 15:38:50 +0100

    Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'TudyTahm' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TudyTahm');
+    expect(app.title).toEqual('Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TudyTahm');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular');
   });
 });
