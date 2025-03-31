6c0af3fc5a968ce88b3cf504f0ba0ad885c50ddc
JezisKristus <vlasaktonda11@gmail.com>
2025-03-31 16:14:13 +0200

    danej lokálně leaflet, pořád nefachá hovado

--- ANG/TudyTahmTahm/src/app/components/map/map.component.ts
+++ ANG/TudyTahmTahm/src/app/components/map/map.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';  // Correct import
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// Import the required classes from the Leaflet library
+import * as L from 'leaflet';  // This imports the entire Leaflet library, including its types
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  private map: L.Map;
-  private layer: L.TileLayer;
-  private marker: L.Marker;
+export class MapComponent implements OnInit, OnDestroy {
+  public map: L.Map;  // Correct type for Leaflet Map
+  private layer: L.TileLayer;  // Tile layer type
+  private marker: L.Marker;  // Marker type
 
   ngOnInit(): void {
-    this.map = L.map('map', {
-      center: [51.958, 9.141],
-      zoom: 10
-    });
+    this.map = L.map('map').setView([51.505, -0.09], 13); // Initialize map with a view
 
-    this.layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-    this.layer.addTo(this.map);
+    // Add OpenStreetMap tile layer
+    this.layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
 
-    this.marker = L.marker([51.958, 9.141]);
-    this.marker.addTo(this.map);
+    // Add a marker to the map
+    this.marker = L.marker([51.505, -0.09]).addTo(this.map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  }
+
+  ngOnDestroy(): void {
+    if (this.map) {
+      this.map.remove(); // Clean up the map instance when the component is destroyed
+    }
   }
 }
--- ANG/TudyTahmTahm/src/app/pages/map-page/map-page.component.spec.ts
+++ ANG/TudyTahmTahm/src/app/pages/map-page/map-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MapPageComponent } from './map-page.component';
 
 describe('MapPageComponent', () => {
