4fe775eeeb945b25996bf7efa282e99dce4f45b6
vlasakantonin <vlasakantonin@sssvt.cz>
2025-04-02 13:13:20 +0200

    Sidebar + neni dodělanej ale je jako komponent

--- ANG/TudyTahmTahm/src/app/pages/map-page/map-page.component.ts
+++ ANG/TudyTahmTahm/src/app/pages/map-page/map-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import {MapComponent} from '../../components/map/map.component';
+import {SidebarComponent} from '../../components/sidebar/sidebar.component';
+
 @Component({
   selector: 'app-map-page',
-  imports: [MapComponent],
+  imports: [MapComponent,SidebarComponent],
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.scss'
 })
---
+++ ANG/TudyTahmTahm/src/app/pages/sing-in-page/sing-in-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sing-in-page',
+  imports: [],
+  templateUrl: './sing-in-page.component.html',
+  styleUrl: './sing-in-page.component.scss'
+})
+export class SingInPageComponent {
+
+}
