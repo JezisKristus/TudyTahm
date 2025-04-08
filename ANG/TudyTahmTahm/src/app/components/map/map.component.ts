e0f284659ec55a9794c2347132256018257e1f9d
JezisKristus <vlasaktonda11@gmail.com>
2025-04-08 08:45:16 +0200

    přidání markeru při rmb

--- ANG/TudyTahmTahm/src/app/components/map/map.component.ts
+++ ANG/TudyTahmTahm/src/app/components/map/map.component.ts
@@ -10,7 +10,7 @@ import * as L from 'leaflet';  // This imports the entire Leaflet library, inclu
 export class MapComponent implements OnInit, OnDestroy {
   map : any;
   layer : any;
-  marker : any;
+  private markers: L.Marker[] = []; // Seznam markerů
 
   ngOnInit(): void {
     this.map = L.map('map').setView([49.8022514,15.6252330], 8); // Initialize map with a view
@@ -19,10 +19,21 @@ export class MapComponent implements OnInit, OnDestroy {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    // Add a marker to the map
-    this.marker = L.marker([50.1192600, 14.4918975]).addTo(this.map)
-      .bindPopup('Your custom marker')
-      .openPopup();
+    // Přidání události na pravé tlačítko myši
+    this.map.on('contextmenu', (event: L.LeafletMouseEvent) => {
+      this.addMarker(event.latlng); // Přidání markeru na mapu
+    });
+  }
+
+  private addMarker(latlng: L.LatLng): void {
+    // Vytvoření nového markeru
+    const marker = L.marker(latlng).addTo(this.map);
+
+    // Přidání markeru do seznamu
+    this.markers.push(marker);
+
+    console.log('Marker přidán:', marker);
+    console.log('Aktuální seznam markerů:', this.markers);
   }
 
   ngOnDestroy(): void {
--- ANG/TudyTahmTahm/src/app/pages/journey-page/journey-page.component.ts
+++ ANG/TudyTahmTahm/src/app/pages/journey-page/journey-page.component.ts
@@ -41,7 +41,6 @@ export class JourneyPageComponent implements OnInit {
         })
       ]
     };
-
     this.updatePath();
   }
 
