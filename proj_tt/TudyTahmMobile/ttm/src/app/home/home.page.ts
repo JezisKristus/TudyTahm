846630378e6aff77a9536f7dd1977ae22632e3a9
deltaplana <deltaplana2@gmail.com>
2025-06-09 22:29:35 +0200

    API I CALL UPON THEE

--- proj_tt/TudyTahmMobile/ttm/src/app/home/home.page.ts
+++ proj_tt/TudyTahmMobile/ttm/src/app/home/home.page.ts
@@ -1,43 +1,60 @@
-// import { Component } from '@angular/core';
-// import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton } from '@ionic/angular/standalone';
-
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: 'home.page.html',
-//   styleUrls: ['home.page.scss'],
-//   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
-// })
-// export class HomePage {
-//   constructor() {}
-// }
-
-
-
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonButton,
+  IonItem, IonLabel, IonInput, IonSpinner, IonText
+} from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput, CommonModule, FormsModule, IonicModule],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonButton, IonItem, IonLabel, IonInput,
+    IonSpinner, IonText,
+    CommonModule, FormsModule, IonicModule
+  ],
 })
 export class HomePage {
   username = '';
   password = '';
+  loading = false;
+  errorMessage: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   login() {
-    if (this.username === 'Beel' && this.password === 'hell666') {
-      this.router.navigate(['/map']);
-    } else {
-      alert('Invalid credentials');
-    }
+    this.loading = true;
+    this.errorMessage = '';
+
+    const body = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post<{ token: string; user: any }>('http://localhost:5000/api/auth/login', body).subscribe({
+      next: (res) => {
+        this.loading = false;
+
+        // TODO: Save token/user in storage or service if needed
+        console.log('Login successful:', res);
+
+        this.router.navigate(['/map']);
+      },
+      error: (err: HttpErrorResponse) => {
+        this.loading = false;
+        if (err.status === 401) {
+          this.errorMessage = 'Invalid username or password.';
+        } else {
+          this.errorMessage = 'Login failed. Please try again.';
+        }
+      }
+    });
   }
 }
